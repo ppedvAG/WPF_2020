2c146e58b08d889e8c8cfdb308d69d85ce0e1e1f
André <ruland@outlook.de>
2020-10-20 15:39:07 +0200

    Mit MultiBinding + Converter

---
+++ HalloBinding/HalloBinding/SliderToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HalloBinding
+{
+    internal class SliderToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new LinearGradientBrush(Color.FromRgb(System.Convert.ToByte(values[0]), 
+                                                         System.Convert.ToByte(values[1]),
+                                                         System.Convert.ToByte(values[2])),
+                                           Colors.Gray, 90);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
