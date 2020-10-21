5850e236376540d4839f8579eb4c145e3791c4ec
André <ruland@outlook.de>
2020-10-21 10:48:42 +0200

    Hallo TPL und Delegates

---
+++ HalloTPL/HalloTPL/HalloDelegate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloTPL
+{
+    delegate void EinfacherDelegate();
+    delegate void DelegateMitPara(string txt);
+    delegate long CalcDelegate(int x, int y);
+
+    class HalloDelegate
+    {
+        public HalloDelegate()
+        {
+            EinfacherDelegate meinDele = EinfacheMethode;
+            Action meinDeleAlsAction = EinfacheMethode;
+            Action meinDeleAlsActionAno = delegate () { Console.WriteLine("Hallo"); };
+            Action meinDeleAlsActionAno2 = () => { Console.WriteLine("Hallo"); };
+            Action meinDeleAlsActionAno3 = () => Console.WriteLine("Hallo");
+
+            DelegateMitPara deleMitPara = MethodeMitPara;
+            Action<string> deleMitParaAlsAction = MethodeMitPara;
+            DelegateMitPara deleMitParaAno = (string txt) => { Console.WriteLine(txt); };
+            DelegateMitPara deleMitParaAno2 = (txt) => Console.WriteLine(txt);
+            DelegateMitPara deleMitParaAno3 = x => Console.WriteLine(x);
+
+            CalcDelegate calcDele = Multi;
+            Func<int, int, long> calcFunc = Sum;
+            Func<int, int, long> calcFuncAno = (int a, int b) => { return a + b; };
+            Func<int, int, long> calcFuncAno2 = (a, b) => { return a + b; };
+            Func<int, int, long> calcFuncAno3 = (a, b) => a + b;
+
+            Predicate<string> immerBool = ImmerBool;
+
+            List<string> texte = new List<string>();
+            texte.Where(x => x.StartsWith("b"));
+            texte.Where(Filter);
+        }
+
+        private bool Filter(string arg)
+        {
+            if (arg.StartsWith("b"))
+                return true;
+            else
+                return false;
+        }
+
+        private bool ImmerBool(string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private long Multi(int x, int y)
+        {
+            return x * y;
+        }
+
+        private long Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        void MethodeMitPara(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        void EinfacheMethode()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
---
+++ HalloTPL/HalloTPL/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HalloTPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Hallo TPL ***");
+
+            //Parallel.Invoke(Zähle, () => Zähle(), Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle);
+            //Parallel.For(0, 100_000, i => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
+
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine("T1 gestartet");
+                Thread.Sleep(800);
+                throw new ExecutionEngineException();
+                Console.WriteLine("T1 fertig");
+            });
+
+            t1.ContinueWith(t => { Console.WriteLine("T1 Continue"); });
+            t1.ContinueWith(t => { Console.WriteLine("T1 OK"); },TaskContinuationOptions.OnlyOnRanToCompletion);
+            t1.ContinueWith(t => { Console.WriteLine($"T1 ERROR {t.Exception.InnerException.Message}"); },TaskContinuationOptions.OnlyOnFaulted);
+
+            Task<long> t2 = new Task<long>(() => 
+            {
+                Console.WriteLine("T2 gestartet");
+                Thread.Sleep(1200);
+                Console.WriteLine("T2 fertig");
+                return 456789345678;
+            });
+
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine($"T2 Result: {t2.Result}"); // ---> t2.Wait();
+
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+
+        private static void Zähle()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}");
+            }
+        }
+    }
+}
