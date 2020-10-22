f1174e3cc12aead7890a608bc8ef4119f6572a5f
André <ruland@outlook.de>
2020-10-22 16:44:37 +0200

    INotifyPropertyChanged

--- ppedv.BooksTracker/ppedv.BooksTracker.UI.WPF/ViewModel/BooksViewModel.cs
+++ ppedv.BooksTracker/ppedv.BooksTracker.UI.WPF/ViewModel/BooksViewModel.cs
@@ -3,17 +3,50 @@ using ppedv.BooksTracker.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace ppedv.BooksTracker.UI.WPF.ViewModel
 {
-    class BooksViewModel
+    class BooksViewModel : INotifyPropertyChanged
     {
         Core core = new Core();
+        private Book selectedBook;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public ObservableCollection<Book> Booklist { get; set; } = new ObservableCollection<Book>();
 
-        public Book SelectedBook { get; set; }
+        public Book SelectedBook
+        {
+            get => selectedBook;
+            set
+            {
+                selectedBook = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedBook)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public string Alter
+        {
+            get
+            {
+                if (SelectedBook == null)
+                    return "---";
+
+                // Save today's date.
+                var today = DateTime.Today;
+
+                // Calculate the age.
+                var age = today.Year - SelectedBook.Published.Year;
+
+                // Go back to the year in which the person was born in case of a leap year
+                if (SelectedBook.Published.Date > today.AddYears(-age)) age--;
+
+                return $"Alter: {age}";
+            }
+        }
 
         public SaveCommand Save { get; set; }
         public ICommand SaveCommand { get; set; }
@@ -23,7 +56,7 @@ namespace ppedv.BooksTracker.UI.WPF.ViewModel
         public BooksViewModel()
         {
             Booklist = new ObservableCollection<Book>(core.Repository.GetAll<Book>());
-            
+
             Save = new SaveCommand(core);
 
             SaveCommand = new RelayCommand(o => core.Repository.SaveAll());
@@ -37,7 +70,6 @@ namespace ppedv.BooksTracker.UI.WPF.ViewModel
             });
 
             NewCommand = new RelayCommand(UserWantsToAddANewBook);
-
         }
 
         private void UserWantsToAddANewBook(object obj)
