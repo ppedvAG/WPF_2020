2bc4d170788ee4091c993b891da6b7f8f3d6dfc5
André <ruland@outlook.de>
2020-10-22 15:06:01 +0200

    Yeah, ein ViewModel

---
+++ ppedv.BooksTracker/ppedv.BooksTracker.UI.WPF/ViewModel/BooksViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ppedv.BooksTracker.Logic;
+using ppedv.BooksTracker.Model;
+using System.Collections.Generic;
+
+namespace ppedv.BooksTracker.UI.WPF.ViewModel
+{
+    class BooksViewModel
+    {
+        Core core = new Core();
+
+        public List<Book> Booklist { get; set; } = new List<Book>();
+
+        public BooksViewModel()
+        {
+            Booklist = new List<Book>(core.Repository.GetAll<Book>());
+        }
+
+    }
+}
